f6ba27aa3984e750534b059caf04323cadaa8ab9
charlis96 <ciarlohernan@gmail.com>
2022-08-25 16:27:52 -0300

    cambio

--- src/ConsoleApplication/Program.cs
+++ src/ConsoleApplication/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleApplication
     public static class Program
     {
         /// <summary>
-        /// Punto de entrada al programa principal, aquí se construyen 100 instancias del objeto Train.
+        /// Punto de entrada al programa principal, aquí se construyen 100 instancias de la clase Train.
         /// </summary>
         /*
         public static void Main()
@@ -28,7 +28,7 @@ namespace ConsoleApplication
         }
         */
         /// <summary>
-        /// Punto de entrada al programa principal, aquí se construyen 10.000.000 instancias del objeto Train.
+        /// Punto de entrada al programa principal, aquí se construyen 10.000.000 instancias de la clase Train.
         /// </summary>
         /*
         public static void Main()
@@ -39,22 +39,35 @@ namespace ConsoleApplication
                 Console.WriteLine(tren.Count);
             }
         }
-        Al ejecutarse la primera iteración del for, se crea una nueva instancia del objeto Train,
+        Al ejecutarse la primera iteración del for, se crea una nueva instancia de la clase Train,
         la cual es asignada a la variable llamada "tren". Cuando se pasa a la segunda iteración,
-        se crea otra instancia del objeto Train, y se le asigna a la variable tren. La instancia
+        se crea otra instancia de Train, y se le asigna a la misma variable tren. La instancia
         anterior de Train queda ocupando espacio en la memoria, pero no es referenciada por ninguna
         variable. Como tampoco es destruida, llega un momento que se acaba la memoria, entonces
         falla la ejecución.
         */
+        /// <summary>
+        /// Punto de entrada al programa principal, aquí se construyen 10.000.000 instancias de la clase Train.
+        /// </summary>
         public static void Main()
         {
             {
                 Train t1 = new Train("Last Train To London");
                 Train t2 = new Train("Last Train To London");
                 Train t3 = new Train("Runaway Train");
-                Console.WriteLine($"{t3.Count}");
                 Console.WriteLine($"{t1 == t2}, {t2 == t3}");
+                Console.WriteLine("hola");
             }
+            /*
+            El motivo por el cual ninguna de las comparaciones es verdadera es porque se crean 3
+            instancias de la clase Train, y cada una es asignada a una variable distinta.
+            Por más que las variables t1 y t2 apuntan a un objeto cuya identificación es
+            "Last Train To London", se trata de 2 instancias distintas, que ocupan 2 lugares
+            distintos en la memoria. En definitiva, las variables t1 y t2 apuntan a 2 lugares distintos
+            de la memoria. Lo mismo sucede con la variable t3, pero aquí hay menos lugar a
+            error ya que la identificación del objeto al que apunta no es igual a la de las otras
+            variables.
+            */
         }
     }
 }
